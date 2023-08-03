6c11653e50bef69ab70a617f74cf55b616f35fa7
jandos0492 <jandos0492@gmail.com>
2023-08-03 02:37:30 -0400

    Finished with backend also added the post request with nodemailer package feature to send email

---
+++ backend/app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const helmet = require("helmet");
+const nodemailer = require("nodemailer");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
+
+app.use(morgan("dev")); // HTTP request logger
+app.use(cookieParser()); // Parse cookies in requests
+app.use(express.json()); // Parse JSON requests bodies
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded requests bodies
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+};
+
+app.use(cors({ origin: ["http://localhost:3000", "https://manamusic.onrender.com"] }));
+
+// helmet helps set a viriety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+    directives: {
+      defaultSrc: ["self"],
+      connectSrc: ["self", "http://localhost:8080"],
+    }
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from "public" directory
+
+// test route
+const router = require("./routes/index");
+router.get("/test", (req, res) => {
+  res.send("<h1>Test route</h1>")
+});
+
+// send email route (using the nodemailer for that feature)
+
+router.post("/send-email", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+    debug: true, // Enable debugging
+  });
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER,
+    subject: "New message from manamusic app",
+    text: `Name: ${name}\nFrom: ${email}\nMessage: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Error sending email: " +error.message);
+  }
+})
+
+app.use(router);
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server started on port http://localhost:${port}`));
+
+module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,35 @@
-const express = require(express);
+const express = require("express");
 
 const router = express.Router();
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  // Serve the frontend's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NOVE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
+    // res. cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(201).json({});
+  });
+}
+
+module.exports = router;
